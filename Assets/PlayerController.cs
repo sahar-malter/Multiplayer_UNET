b3928b525e4f41c8daba2628fe4447f782afd981
sahar <sahar.malter1@gmail.com>
2021-04-06 11:56:09 +0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+    public Rigidbody RB;
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        RB.AddForce(transform.forward * translation, ForceMode.VelocityChange);
+        RB.AddTorque(0, rotation, 0, ForceMode.Acceleration);
+        //transform.Translate(0, 0, translation);
+        //transform.Rotate(0, rotation, 0);
+
+    }
+
+}
---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            gameObject.GetComponent<PlayerController>().enabled = true;
+            gameObject.GetComponentInChildren<Camera>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<PlayerController>().enabled = false;
+            gameObject.GetComponentInChildren<Camera>().enabled = false;
+        }
+
+    }
+}
